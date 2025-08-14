bb4d9ae0143d467d2f17b638ccf3b58f9c3a28be
satyam <mishrasatyam9890@gmail.com>
2025-08-14 21:18:54 +0530

    Shoe Page Completed

--- lib/pages/shoe_page.dart
+++ lib/pages/shoe_page.dart
@@ -9,39 +9,71 @@ class ShoePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    TextButton.icon(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      label: Icon(Icons.arrow_back, size: 40),
-                    ),
-                  ],
-                ),
-                Image.network(card.image),
-                Padding(
-                  padding: EdgeInsetsGeometry.all(18),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text(card.product_name), Text(card.description)],
-                  ),
-                ),
-                BottomAppBar(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Row(
                     children: [
-                      Text(card.price.toString()),
-                      Icon(Icons.shopping_cart_outlined),
+                      TextButton.icon(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        label: Icon(Icons.arrow_back, size: 40),
+                      ),
                     ],
                   ),
-                ),
-              ],
+                  Image.network(card.image),
+                  Padding(
+                    padding: EdgeInsetsGeometry.all(18),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          card.product_name,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                          ),
+                        ),
+                        Text(card.description, 
+                        style: TextStyle(
+                        fontSize: 30,
+                      ),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.white),
+        padding: EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '\$' + card.price.toString(),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color(0xFF2193b0),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Icon(Icons.shopping_cart_outlined, color: Colors.white),
             ),
           ],
         ),
